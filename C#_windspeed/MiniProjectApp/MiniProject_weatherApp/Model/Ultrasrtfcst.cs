9a6297793f9a3a89ad2a2612c4966d0b1ccd7e09
YoungHunPark0 <ekddusgkwl12@naver.com>
2023-05-12 17:21:17 +0900

    풍속, 습도, 강수량 UI

---
+++ C#_windspeed/MiniProjectApp/MiniProject_weatherApp/Model/Ultrasrtfcst.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniProject_weatherApp.Model
+{
+    public class Ultrasrtfcst
+    {
+        public string RN1 { get; set; }
+        public int REH { get; set; }
+        public int VEC { get; set;}
+        public int WSD { get; set;}
+    }
+}
