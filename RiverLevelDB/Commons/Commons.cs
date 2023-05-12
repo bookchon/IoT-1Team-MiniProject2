614a730c315faa745a4d371a47b8fc1fa46e9c95
drowned <87066883+drown-ed@users.noreply.github.com>
2023-05-12 17:24:42 +0900

    Add files via upload

---
+++ RiverLevelDB/Commons/Commons.cs
@@ -0,0 +1,13 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+using System.Threading.Tasks;
+
+namespace RiverLevelDB.Logics
+{
+    public class Commons
+    {
+        public static readonly string connString = "Server=localhost;" +
+                                                    "Port=3306;" +
+                                                    "Database=miniproject01;" +
+                                                    "Uid=root;" + "Pwd=1234;";
+    }
+}
