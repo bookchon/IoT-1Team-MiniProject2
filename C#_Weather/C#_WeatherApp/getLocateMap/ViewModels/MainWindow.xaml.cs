a5cef0ac3b9ac834e4860d3e80e6cf527a3365a9
PARK SEONG HYEON <lovehyun95@gmail.com>
2023-05-12 17:29:23 +0900

    Update MainWindow.xaml.cs

--- C#_Weather/C#_WeatherApp/getLocateMap/ViewModels/MainWindow.xaml.cs
+++ C#_Weather/C#_WeatherApp/getLocateMap/ViewModels/MainWindow.xaml.cs
@@ -24,7 +24,7 @@ namespace getLocateMap
             string strHtml = File.ReadAllText(map_url);
             Debug.WriteLine(strHtml);
             string htmlRoadView = $@"https://map.kakao.com/?panoid=1042459733&pan=176.0&zoom=0&map_type=TYPE_MAP&map_attribute=ROADVIEW&urlX=523869.0&urlY=1084106.0";
-            browser.LoadHtml(htmlRoadView, "http://www.team-one.com/");
+            browser.LoadHtml(strHtml, "http://www.team-one.com/");
         }
     }
 }
