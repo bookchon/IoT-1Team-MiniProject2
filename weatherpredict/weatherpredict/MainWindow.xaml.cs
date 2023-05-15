ccb987a62105dd063de4e6943a8ffb5caaf7e571
bookchon <realtansan@gmail.com>
2023-05-15 11:12:15 +0900

    2023. 5. 15.

--- weatherpredict/weatherpredict/MainWindow.xaml.cs
+++ weatherpredict/weatherpredict/MainWindow.xaml.cs
@@ -1,26 +1,16 @@
-﻿using MySqlConnector;
+﻿using MahApps.Metro.Controls;
+using MySql.Data.MySqlClient;
 using System;
-using System.Collections.Generic;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace weatherpredict
 {
     /// <summary>
     /// MainWindow.xaml에 대한 상호 작용 논리
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
         public MainWindow()
         {
